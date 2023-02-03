131fd74cdf8c3061d70a05453ad734f2ed9f26a5
HenryBondah <bondahdean@gmail.com>
2023-02-02 23:08:38 -0700

    aa

--- Chamber/scripts/scripts.js
+++ Chamber/scripts/scripts.js
@@ -27,5 +27,16 @@ function displayYear(x) {
 }
 
 displayYear(years)
-years.map(displayYear)
+
 document.querySelector("#lastmodified").textContent = document.lastModified;
+
+// Show the meet-greet message on Tue/Thu
+// Date.getDay() uses a 0 based index to return the day of the week
+var messagedate = new Date();
+console.log(messagedate.getDay())
+if (messagedate.getDay()==2 || messagedate.getDay()==4){
+    document.querySelector("#meet-greet").classList.add('active');
+} else {
+    document.querySelector("#meet-greet").style.display = "none";
+
+}
---
+++ Chamber/styles/main.js
@@ -0,0 +1,4 @@
+var messagedate = new Date();
+if (messagedate.getDay()==2 || messagedategetDate.getDay()==4){
+    document.querySelector("#meet-greet").classList.add('active')
+}
